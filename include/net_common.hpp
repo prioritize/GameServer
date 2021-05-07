698da93be636f46a387c52fb7b5c7bafb40705d2
Jared Leach <leach.jared@gmail.com>
2021-05-06 18:20:06 -0700

    Moving to desktop

---
+++ include/net_common.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <deque>
+#include <optional>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cstdint>
+
+#include <boost/asio.hpp>
+#include <boost/asio/ts/buffer.hpp>
+#include <boost/asio/ts/internet.hpp>
